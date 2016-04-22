0bf673077b2e50a3dd9922b89aaa91e520f92df4
ahmarkhanpathan <ahmarkhan_p@yahoo.com>
2016-04-22 20:28:26 +0530

    JavaSession1 Assignment3

---
+++ src/Package3/Assignment3.java
@@ -0,0 +1,23 @@
+package Package3;
+
+public class Assignment3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//this Code shows two examples of data that 
+		//cannot be stored in a variable of type int
+	
+		int a,b;
+		
+		a= 2.25;
+			//variable a of type integer cannot be stored as  a float
+		b= "Name"
+		   // variable b of type integer cannot be stored as a string
+		
+		
+		
+	}
+
+}
